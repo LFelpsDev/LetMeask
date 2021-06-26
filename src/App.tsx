92ce804e9e859707334a87b294459f97f1990e87
lufegs <felipe-mara2003@hotmail>
2021-06-25 22:07:12 -0300

    aula 4 finalizada

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import {Home} from './pages/Home';
 import { NewRoom } from './pages/NewRoom';
+import { AdminRoom } from './pages/AdminRoom'
 
 import { AuthContextProvider } from '../src/contexts/AuthContext'
 import { Room } from './pages/Room';
@@ -15,6 +16,7 @@ function App() {
       <Route path="/" exact component={Home}  /> {/* esse exact quer dizer que a pagina so sera exibida quando estiver exatamente o mesmo caminho, colocado no path */}
       <Route path="/rooms/news"  component={NewRoom} />
       <Route path="/rooms/:id" component={Room} />
+      <Route path="/admin/rooms/:id" component={AdminRoom} />
       </Switch>
       </AuthContextProvider>
     </BrowserRouter>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ import { FormEvent, useState } from 'react';
 import {  useAuth   } from '../hooks/useAuth'
 import { useHistory } from 'react-router-dom';
 
+
 import illustrationImg from '../assets/images/illustration.svg'
 import  logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg'
@@ -43,6 +44,11 @@ export function Home(){
         return;
       }
       
+      if (roomRef.val().endedAt){
+        alert('Room Already closed');
+        return;
+      }
+
       history.push(`/rooms/${roomCode}`)
     }
 
